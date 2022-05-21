2c6f70375ac1b313b1abc0a66e59f574b828e81a
Arsalan Khattak <me@khattak.dev>
2022-05-21 10:26:45 +0500

    auth template

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import classes from "../styles/auth.module.css";
+
+function Login() {
+  return (
+    <div className={classes.body}>
+      <h1>Login</h1>
+    </div>
+  );
+}
+export default Login;
---
+++ pages/signup.js
@@ -0,0 +1,11 @@
+import classes from "../styles/auth.module.css";
+
+function Signup() {
+  return (
+    <div className={classes.body}>
+      <h1>Signup</h1>
+    </div>
+  );
+}
+
+export default Signup;
